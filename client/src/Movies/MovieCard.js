bfff60ca98e286e164484973b507c86595d586b8
mlazzell17@gmail.com <MicheSi>
2019-12-17 14:35:06 -0700

    changed code on moviecard

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,11 +1,12 @@
 import React from 'react';
-
+import Movie from './Movie';
 
 const MovieCard = ({movie}) => {
   const { title, director, metascore, stars } = movie;
 
   return (
-    <div className="movie-card">
+    <div className="save-wrapper">
+      <div className="movie-card">
         <h2>{title}</h2>
         <div className="movie-director">
           Director: <em>{director}</em>
@@ -21,7 +22,9 @@ const MovieCard = ({movie}) => {
           </div>
         ))}
       </div>
-  )
+      <div className="save-button">Save</div>
+    </div>
+  );
 };
 
 export default MovieCard;
